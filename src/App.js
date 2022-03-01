146515a8bc72a1f7bbe5d167e5a813a673157ac9
Nina <n.roethinger@outlook.fr>
2022-03-01 05:01:52 +0100

    Movies app from JustFS Tutorial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserList from "./pages/UserList";
+
+const App = () => {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/coup-de-coeur' element={<UserList />}></Route>
+          <Route path='/*' element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+  );
+};
+
+export default App;
